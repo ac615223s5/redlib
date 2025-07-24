5abd8297f3cf05c797b2b6b1fc5b14d21b80235a
nieve <ppu7haxj@duck.com>
2025-07-24 02:16:41 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -233,8 +233,8 @@ async fn main() {
 		.at("/highlighted.js")
 		.get(|_| resource(include_str!("../static/highlighted.js"), "text/javascript", false).boxed());
 	app
-	.at("/commentcollapse.js")
-	.get(|_| resource(include_str!("../static/commentcollapse.js"), "text/javascript", false).boxed());
+		.at("/commentcollapse.js")
+		.get(|_| resource(include_str!("../static/commentcollapse.js"), "text/javascript", false).boxed());
 
 	// Proxy media through Redlib
 	app.at("/vid/:id/:size").get(|r| proxy(r, "https://v.redd.it/{id}/DASH_{size}").boxed());
